0d8bc20e6c5656f159b9959b18b239244236f60e
calebosam <caleb.osam@amalitech.com>
2023-03-17 00:50:11 +0000

    feat: use all routers and middleware in app

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-const express = require("express")
-const logger = require("morgan")
-const cors = require("cors")
-const bodyParser = require("body-parser")
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const uploadFileRouter = require("./routers/uploadFileRouter");
+const uploadFileRouterV3 = require("./routers/uploadFileRouterV3");
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(logger("dev"));
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(logger("dev"))
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello world</h1>");
+});
 
-app.get("/", (req, res, next)=>{
-    res.send("<h1>Hello world</h1>")
-})
+app.use("/api/v2/aws", uploadFileRouter);
+app.use("/api/v3/aws", uploadFileRouterV3);
+
 //Error Handler
 app.use((err, req, res, next) => {
-    if (err) {
-      if (process.env.NODE_ENV === "production") {
-        res.status(400).json({ status: "fail", message: "Bad request" });
-      } else {
-        console.log(err);
-        res.status(err.statusCode).json({
-          status: err.status,
-          message: err.message,
-          error: err,
-          stack: err.stack,
-        });
-      }
+  if (err) {
+    if (process.env.NODE_ENV === "production") {
+      res.status(400).json({ status: "fail", message: "Bad request" });
+    } else {
+      console.log(err);
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack,
+      });
     }
-  });
-  
+  }
+});
 
-module.exports = app
+module.exports = app;
